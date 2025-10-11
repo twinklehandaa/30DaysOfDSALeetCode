7c2155bd4e2ab5e81bebd261b46a8fa04540bddf
Twinkle Handa <twinklehandaa@gmail.com>
2025-10-11 23:59:36 +0530

    Day 10

---
+++ 2AddTwoNumbers.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode sumoflist = new ListNode(0);
+        ListNode temp = sumoflist;
+        int carry = 0;
+
+        while (l1!=null || l2!=null){
+            int x = (l1!=null)?l1.val:0;
+            int y = (l2!=null)?l2.val:0;
+
+            int sum = x + y + carry;
+            carry = (sum/10);
+
+            temp.next = new ListNode(sum%10);
+            temp = temp.next;
+            
+            if (l1!=null){
+                l1 = l1.next;
+            }
+            if (l2!=null){
+                l2 = l2.next;
+            }
+        }
+        if (carry>0) temp.next=new ListNode(carry);
+        return sumoflist.next;
+    }
+}
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
