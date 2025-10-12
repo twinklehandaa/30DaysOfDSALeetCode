013196a2e9966efb40f7e4d585bafa02dfd96b1f
Twinkle Handa <twinklehandaa@gmail.com>
2025-10-13 01:31:24 +0530

    2469

---
+++ day12/ConvertTheTemperaure.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double k = celsius + 273.15;
+        double f = celsius * 1.80 + 32.00;
+        double[] temp = {k,f};
+        return temp;
+    }
+}
