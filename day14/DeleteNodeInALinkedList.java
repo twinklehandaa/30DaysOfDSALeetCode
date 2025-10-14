a787a9196292424c2664004588792ec84a105154
Twinkle Handa <twinklehandaa@gmail.com>
2025-10-14 22:57:38 +0530

    237

---
+++ day14/DeleteNodeInALinkedList.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteNode(ListNode node) {
+        node.val = node.next.val; // copy the value of next node
+        node.next = node.next.next; // skip the node
+        return node;
+    }
+}
+
+// Input: head = [4,5,1,9], node = 5
+// Output: [4,1,9]
+// Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
