61b1e5d367703c884733d376200c04cd67d48ce6
Twinkle Handa <twinklehandaa@gmail.com>
2025-10-14 22:14:44 +0530

    48

---
+++ day14/RotateImage.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i<matrix.length; i++){
+            for (int j = i+1; j<matrix.length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i<matrix.length; i++){
+            int left = 0; //reverse using two pointer
+            int right = matrix.length-1;
+            while (left<right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
+
+// Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+// Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
