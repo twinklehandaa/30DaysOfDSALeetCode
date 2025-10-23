4415c9a0ec0e49b2b49aed2618c365287b596132
Twinkle Handa <twinklehandaa@gmail.com>
2025-10-23 11:43:48 +0530

    275

---
+++ day22/H-IndexII.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = 0; int n = citations.length;
+        for (int i = 0; i<n; i++){
+            int x = n-i;
+            if (citations[i]>=x) {
+                h = x;
+                break;
+            }
+        }
+        return h;
+    }
+}
+
+// Input: citations = [0,1,3,5,6]
+// Output: 3
+// Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each of them had received 0, 1, 3, 5, 6 citations respectively.
+// Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3
