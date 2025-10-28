3943d97bbbde16a07eb04bb25a57089449d7418e
Twinkle Handa <twinklehandaa@gmail.com>
2025-10-28 19:36:21 +0530

    2161

---
+++ day26/PartitionArrayAccordingToGivenPivot.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int[] result = new int[nums.length];
+
+        int i = 0;
+        for (int n: nums) if (n < pivot) result[i++] = n;  // add smaller elements first
+        for (int n: nums) if (n == pivot) result[i++] = n; // add pivot element after smaller
+        for (int n: nums) if (n > pivot) result[i++] = n; // add larger elements after pivot
+
+        return result;
+
+    }
+}
+
+// Input: nums = [9,12,5,10,14,3,10], pivot = 10
+// Output: [9,5,3,10,10,12,14]
+// Explanation: 
+// The elements 9, 5, and 3 are less than the pivot so they are on the left side of the array.
+// The elements 12 and 14 are greater than the pivot so they are on the right side of the array.
+// The relative ordering of the elements less than and greater than pivot is also maintained. [9, 5, 3] and [12, 14] are the respective orderings.
